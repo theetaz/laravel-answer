b4fb2dd0e55f74b19dffcdc0673b1dff8e6fffdd
Nipun Theekshana <theekshana2@gmail.com>
2018-10-09 18:32:04 +0530

    answer model and question table column rename

--- app/Question.php
+++ app/Question.php
@@ -31,7 +31,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if($this->answers > 0){
+        if($this->answers_count > 0){
 
             if($this->best_answer_id){
                 return "answer-accepted";
@@ -45,5 +45,10 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 }
--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
