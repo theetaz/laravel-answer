0b59ea13b98c377671d03b3f54d30acf29bc7169
Nipun Theekshana <theekshana2@gmail.com>
2018-10-10 01:46:50 +0530

    crete answer for questions

---
+++ resources/views/answers/_index.blade.php
@@ -0,0 +1,45 @@
+<div class="row">
+    <div class="col-md-12 mt-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <h2>{{ $answers_count ." ". str_plural('Answer', $answers_count) }}</h2>
+                </div>
+                <hr/>
+                @include('layouts._messages')
+                @foreach($answers as $answer)
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This answer is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1350</span>
+                            <a title="This answer is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark this as best answer (click again to undo)" class="vote-accepted mt-2">
+                                <i class="fas fa-check fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $answer->body !!}
+
+                            <div class="float-right mt-3">
+                                <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $answer->user->url }}" class="pr-2">
+                                        <img src="{{ $answer->user->avatar }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -49,50 +49,7 @@
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-md-12 mt-4">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title">
-                            <h2>{{ $question->answers_count ." ". str_plural('Answer', $question->answers_count) }}</h2>
-                        </div>
-                        <hr/>
-                        @foreach($question->answers as $answer)
-                            <div class="media">
-                                <div class="d-flex flex-column vote-controls">
-                                    <a title="This answer is useful" class="vote-up">
-                                        <i class="fas fa-caret-up fa-3x"></i>
-                                    </a>
-                                    <span class="votes-count">1350</span>
-                                    <a title="This answer is not useful" class="vote-down off">
-                                        <i class="fas fa-caret-down fa-3x"></i>
-                                    </a>
-                                    <a title="Click to mark this as best answer (click again to undo)" class="vote-accepted mt-2">
-                                        <i class="fas fa-check fa-2x"></i>
-                                    </a>
-                                </div>
-                                <div class="media-body">
-                                    {!! $answer->body !!}
-
-                                    <div class="float-right mt-3">
-                                        <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                                        <div class="media mt-2">
-                                            <a href="{{ $answer->user->url }}" class="pr-2">
-                                                <img src="{{ $answer->user->avatar }}">
-                                            </a>
-                                            <div class="media-body mt-1">
-                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr/>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('answers._index', ['answers' => $question->answers, 'answers_count' => $question->answers_count])
+        @include('answers._create')
     </div>
 @endsection
